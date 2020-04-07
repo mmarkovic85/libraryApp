95e265db78a8049dacde6a33ed28a7a0af25e8d6
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-07 20:49:34 +0200

    added nodemon (front/back) support

--- src/components/Placeholder.js
+++ src/components/Placeholder.js
@@ -5,6 +5,7 @@ const Placeholder = () => (
   <div>
     <h1>Nineveh</h1>
     <p>Lorem ipsum placeholder from React with dev-server</p>
+    <p>Nodemon is building frontend bundle.js :D</p>
   </div>
 );
 
