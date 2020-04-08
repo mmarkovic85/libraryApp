0a00e161c18bbdd6ffd4f6684f7e7cac6eade074
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-08 17:53:45 +0200

    added user schema properties

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,11 +1,39 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: {},
-  username: {},
-  password: {},
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "E-mail field is required"],
+    maxlength: [30, "E-mail field must be 50 characters or less"],
+    trim: true,
+    lowercase: true,
+    validate: {
+      // TODO
+    }
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: [true, "Username field is required"],
+    minlength: [3, "Username field must be 3 characters or more"],
+    maxlength: [40, "Username field must be 40 characters or less"],
+    trim: true
+  },
+  password: {
+    type: String,
+    required: [true, "Password field is required"],
+    minlength: [8, "Password field must be 8 characters or more"],
+    maxlength: [20, "Password field must be 20 characters or less"],
+    validate: {
+      // TODO
+    }
+  },
   tokens: [{
-    token: {}
+    token: {
+      type: String,
+      required: true
+    }
   }]
 }, {
   timestamps: true
