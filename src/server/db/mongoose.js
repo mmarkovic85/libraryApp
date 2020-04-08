d8df652e74e2796d5462ca41c4a99d6f79e46ee6
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-08 17:25:58 +0200

    db connection err handler

--- src/server/db/mongoose.js
+++ src/server/db/mongoose.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 // Connect to database
-mongoose.connect(
-  process.env.MONGODB_URL,
-  {
+mongoose
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  }
-);
+  })
+  .catch(er => console.log("Mongoose connection error: ", er.message));
