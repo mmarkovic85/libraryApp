2e2b8460ea630af636970716c623878aee5b7f60
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-08 13:57:15 +0200

    added book schema properties

--- src/server/models/book.js
+++ src/server/models/book.js
@@ -1,17 +1,57 @@
 const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
-  author: {},
-  title: {},
-  year: {},
-  genre: {},
-  publisher: {},
-  language: {},
-  isbn: {},
-  notes: {},
+  author: {
+    type: String,
+    maxlength: [30, "Author field must be 30 characters or less"],
+    required: [true, "Author field is required"],
+    trim: true
+  },
+  title: {
+    type: String,
+    maxlength: [50, "Title field must be 50 characters or less"],
+    required: [true, "Title field is required"],
+    trim: true
+  },
+  year: {
+    type: String,
+    validate() {
+      // TODO:
+      // length == 4
+      // min - year of first printed book
+      // max - current year
+    }
+  },
+  genre: {
+    type: String,
+    maxlength: [50, "Genre field must be 50 characters or less"],
+    trim: true
+  },
+  publisher: {
+    type: String,
+    maxlength: [50, "Publisher field must be 50 characters or less"],
+    trim: true
+  },
+  language: {
+    type: String,
+    maxlength: [20, "Language field must be 20 characters or less"],
+    trim: true
+  },
+  isbn: {
+    type: String,
+    validate() {
+      // TODO:
+      // length == 10 || 13
+    }
+  },
+  notes: {
+    type: String,
+    maxlength: [255, "Notes field must be 255 characters or less"],
+    trim: true
+  },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    required: true,
+    required: [true, "Owner field is required"],
     ref: "User"
   }
 }, {
