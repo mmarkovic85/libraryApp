ae4b533c3c10a49afabbac097436b3820c5099a9
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-08 11:48:14 +0200

    user model basic structure

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  /** TODO: add schema properties */
+  email: {},
+  username: {},
+  password: {},
+  tokens: [{
+    token: {}
+  }]
 }, {
-  timestamps
+  timestamps: true
 });
+
+userSchema.virtual("books", {
+  ref: "Book",
+  localField: "_id",
+  foreignField: "owner"
+})
 
 const User = mongoose.model("User", userSchema);
 
