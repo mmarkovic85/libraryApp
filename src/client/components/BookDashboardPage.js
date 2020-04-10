132198ac6dca1bf32b1884446cc9b5f140b5f679
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-10 18:07:28 +0200

    BookDashboardPage setup

---
+++ src/client/components/BookDashboardPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookDashboardPage = () => (
+  <p>BooksDashboardPage</p>
+);
+
+export default BookDashboardPage;
--- src/client/routers/AppRouter.js
+++ src/client/routers/AppRouter.js
@@ -5,13 +5,14 @@ import {
   Route
 } from "react-router-dom";
 
+import BookDashboardPage from "../components/BookDashboardPage";
 import LoginPage from "../components/LoginPage";
 
 const AppRouter = () => (
   <Router>
     <Switch>
       <Route path="/" component={LoginPage} exect={true} />
-      <Route path="/dashboard" component={/** TODO Dashboard */} />
+      <Route path="/dashboard" component={BookDashboardPage} />
       <Route conponent={/** TODO Page404 */} />
     </Switch>
   </Router>
