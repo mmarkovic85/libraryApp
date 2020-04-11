560213d732bcac29295f6afc8ab85e0bc943e912
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-11 20:20:10 +0200

    LoginPage structured

--- src/client/components/LoginPage.js
+++ src/client/components/LoginPage.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const LoginPage = () => (
-  <p>LoginPage</p>
-);
+const LoginPage = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(
+      e.target.username.value,
+      e.target.password.value
+    );
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Login:</legend>
+          <input type="text" name="username" placeholder="username or e-mail" />
+          <br />
+          <input type="password" name="password" placeholder="password" />
+          <br />
+          <button type="submit">Sign in</button>
+        </fieldset>
+      </form>
+      <Link to="/registration">
+        <button>Sign up</button>
+      </Link>
+    </div>
+  );
+};
 
 export default LoginPage;
