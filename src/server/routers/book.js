6c479cf4cc47937f1c6d0e9eace73947f2128c46
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-12 15:57:05 +0200

    added routes to user and book routers

--- src/server/routers/book.js
+++ src/server/routers/book.js
@@ -3,8 +3,12 @@ const express = require("express");
 const router = new express.Router();
 
 // Create
+router.post("/books", async (req, res) => { });
 // Read
+router.get("/books", async (req, res) => { });
 // Update
+router.put("/books", async (req, res) => { });
 // Delete
+router.delete("/books", async (req, res) => { });
 
 module.exports = router;
--- src/server/routers/user.js
+++ src/server/routers/user.js
@@ -3,8 +3,12 @@ const express = require("express");
 const router = new express.Router();
 
 // Create
+router.post("/users", async (req, res) => { });
 // Read
+router.get("/users", async (req, res) => { });
 // Update
+router.put("/users", async (req, res) => { });
 // Delete
+router.delete("/users", async (req, res) => { });
 
 module.exports = router;
