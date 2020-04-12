c9d3cfe7db6724b42f69b5bb8f68493fab2185ab
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-12 16:06:25 +0200

    client router refactor

--- src/server/routers/client.js
+++ src/server/routers/client.js
@@ -3,8 +3,13 @@ const express = require("express");
 
 const router = new express.Router();
 
-router.get("*", (req, res) => res.sendFile(
-  path.join(__dirname, "..", "..", "..", "public", "index.html")
-));
+router.get("*", (req, res) => res.sendFile(path.join(
+  __dirname,
+  "..",
+  "..",
+  "..",
+  "public",
+  "index.html"
+)));
 
 module.exports = router;
