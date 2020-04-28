1ef3eae8968635488268e2cb3cd807bda311a3f4
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-28 21:18:07 +0200

    added test file for user model

---
+++ tests/server/models/user.test.js
@@ -0,0 +1,13 @@
+require("core-js/stable");
+require("regenerator-runtime/runtime");
+const request = require("supertest");
+
+const app = require("../../../src/server/app");
+const User = require("../../../src/server/models/user");
+
+// TODO import fn for beforeEach
+// TODO import fixtures
+
+beforeEach(() => {/** fn - setupDb */ });
+
+test("Should test something", async () => { });
