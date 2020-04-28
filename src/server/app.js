df73b26735b587391e5f37f18a5331bceb391cbc
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-28 17:19:40 +0200

    express app extracted in separate file
    for app's API testing purposes

---
+++ src/server/app.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+require("./db/mongoose");
+
+const bookRouter = require("./routers/book");
+const clientRouter = require("./routers/client");
+const userRouter = require("./routers/user");
+
+const app = express();
+
+// Serve public dir
+app.use(
+  express.static(
+    path.join(__dirname, "..", "..", "public")
+  )
+);
+
+// API routes
+app.use("/api", userRouter);
+app.use("/api", bookRouter);
+// React router
+app.use("/", clientRouter);
+
+module.exports = app;
--- src/server/server.js
+++ src/server/server.js
@@ -1,26 +1,6 @@
-const path = require("path");
-const express = require("express");
-require("./db/mongoose");
+const server = require("./app.js");
 
-const bookRouter = require("./routers/book");
-const clientRouter = require("./routers/client");
-const userRouter = require("./routers/user");
-
-const server = express();
 const port = process.env.PORT || 3000;
-
-// Serve public dir
-server.use(
-  express.static(
-    path.join(__dirname, "..", "..", "public")
-  )
-);
-
-// API routes
-server.use("/api", userRouter);
-server.use("/api", bookRouter);
-// React route
-server.use("/", clientRouter);
 
 // Run server
 server.listen(
