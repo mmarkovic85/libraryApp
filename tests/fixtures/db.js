48dc1e86d0a963c8c2d811513aa223d25bfff134
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-29 23:14:16 +0200

    added fixtures for user db documents

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,7 +1,33 @@
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+const userOne = {
+  _id: new mongoose.Types.ObjectId(),
+  email: "filozof@atina.edu",
+  username: "Sokrat",
+  password: "nistaneznam",
+  tokens: [{
+    token: jwt.sign({ _id: userTwoId._id }, process.env.JWT_SECRET)
+  }]
+};
+
+const userTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  email: "emperor@rome.com",
+  username: "Cezar",
+  password: "princeps",
+  isProfilePrivate: true,
+  tokens: [{
+    token: jwt.sign({ _id: userOneId._id }, process.env.JWT_SECRET)
+  }]
+};
+
 const setupDatabase = async () => {
-  // TODO Db setup before each test
+  await User.deleteMany();
 };
 
 module.exports = {
+  userOne,
+  userTwo,
   setupDatabase
 };
