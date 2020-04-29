31d03ed2455de04ce906665459b7dbcf67de23be
mmarkovic85 <milosmarkovic85@gmail.com>
2020-04-29 23:16:15 +0200

    added fixtures for book db documents

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -22,11 +22,47 @@ const userTwo = {
   }]
 };
 
+const bookOne = {
+  author: "Rodžer Zelazni",
+  title: "Gospodar svetlosti",
+  yaer: "2013",
+  genre: "naučna fantastika",
+  publisher: "ZARDOZ",
+  language: "srpski",
+  notes: "prevod: Aleksandar B. Nedeljković",
+  isBookRead: true,
+  owner: userOne._id
+};
+
+const bookTwo = {
+  author: "Svetislav Basara",
+  title: "Fama o biciklistima",
+  yaer: "2013",
+  publisher: "Laguna",
+  language: "srpski",
+  isbn: "978-86-521-1090-2",
+  owner: userOne._id
+};
+
+const bookThree = {
+  author: "Sentimentalna povest Britanskog carstva",
+  title: "Borislav Pekić",
+  yaer: "2006",
+  publisher: "Solaris",
+  language: "srpski",
+  isBookRead: true,
+  owner: userTwo._id
+};
+
 const setupDatabase = async () => {
   await User.deleteMany();
+  await Book.deleteMany();
 };
 
 module.exports = {
+  bookOne,
+  bookTwo,
+  bookThree,
   userOne,
   userTwo,
   setupDatabase
