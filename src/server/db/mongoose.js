d151f2f99dc29318c83fbb9ee0e6915dd62f2ee8
mmarkovic85 <milosmarkovic85@gmail.com>
2020-05-06 20:09:42 +0200

    (api) book router update

--- src/server/db/mongoose.js
+++ src/server/db/mongoose.js
@@ -5,6 +5,7 @@ mongoose
   .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .catch(er => console.log("Mongoose connection error: ", er.message));
--- src/server/routers/book.js
+++ src/server/routers/book.js
@@ -2,38 +2,60 @@ const express = require("express");
 
 const auth = require("../middleware/user-auth");
 const Book = require("../models/book");
+const { isValidBookUpd } = require("../util/validator");
 
 const router = new express.Router();
 
 // Create new book
 router.post("/books", auth, async (req, res) => {
   try {
+    // Create and save new book
     const book = new Book({
       ...req.body,
       owner: req.user._id
     });
     await book.save();
+    // Send response
     res.status(201).send(book);
   } catch (e) {
     res.status(400).send({ error: e.message });
   };
 });
+
 // Read
 router.get("/books", async (req, res) => { });
 
 // Update user's book
-router.put("/books/:id", async (req, res) => { });
+router.put("/books/:id", auth, async (req, res) => {
+  try {
+    const updates = Object.keys(req.body);
+    // Check if updates are valid
+    if (!isValidBookUpd(updates)) throw new Error("Invalid updates!");
+    // Find and update
+    const book = await Book.findOneAndUpdate(
+      { _id: req.params.id, owner: req.user._id },
+      req.body,
+      { new: true }
+    );
+
+    // Send response
+    book ? res.send(book) : res.status(400).send();
+  } catch (e) {
+    res.status(400).send({ error: e.message });
+  };
+});
 
 // Delete user's book
 router.delete("/books/:id", auth, async (req, res) => {
   try {
+    // Find and delete book
     const book = await Book.findOneAndDelete({
       _id: req.params.id,
       owner: req.user._id
     });
-    book ?
-      res.send(book) :
-      res.status(404).send();
+
+    // Syend response
+    book ? res.send(book) : res.status(404).send();
   } catch (e) {
     res.status(500).send();
   };
