663323673643d0bdfcb39addfc659c5748865111
mmarkovic85 <milosmarkovic85@gmail.com>
2020-05-14 21:11:41 +0200

    (front) added logout/logoutall in Header

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,17 +1,65 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
-const Header = () => (
-  <header className="header">
-    <h1>
-      <a href="/">Nineveh</a>
-    </h1>
+import AppContext from "../context/AppContext"
 
-    <div>
-      <a href="/login">Login</a>
-      <span> / </span>
-      <a href="/registration">Sign up</a>
-    </div>
-  </header>
-);
+const Header = () => {
+  const { authToken, setAuthToken } = useContext(AppContext);
+
+  const handleLogout = (e) => {
+    const { name } = e.target;
+    let url;
+
+    switch (name) {
+      case "logout":
+        url = "/api/users/logout"
+        break;
+      case "logoutall":
+        url = "/api/users/logoutall"
+        break;
+    };
+
+    fetch(url, {
+      method: "POST",
+      headers: { "Authorization": "Bearer " + authToken }
+    }).then(res => res.status == "200" && setAuthToken(""))
+      .catch(e => console.log(e.message))
+  };
+
+  return (
+    <header className="header">
+      <h1>
+        <Link to="/">Nineveh</Link>
+      </h1>
+      {
+        authToken ?
+          <div>
+            <button
+              name="logout"
+              onClick={handleLogout}
+            >Logout</button>
+
+            <span> / </span>
+
+            <button
+              name="logoutall"
+              onClick={handleLogout}
+            >Logout All</button>
+          </div> :
+          <div>
+            <Link to="/login">
+              <button>Login</button>
+            </Link>
+
+            <span> / </span>
+
+            <Link to="/registration">
+              <button>Sign Up</button>
+            </Link>
+          </div>
+      }
+    </header>
+  );
+};
 
 export default Header;
